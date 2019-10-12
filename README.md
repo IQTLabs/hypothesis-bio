fe91123fe909fe4c73f9c75f18eec64721e615ac
Benjamin-Lee <Benjamin-Lee@users.noreply.github.com>
2019-10-12 04:28:44 +0000

    Automated deployment: Sat Oct 12 04:28:44 UTC 2019 e9717c97ce5c848f6d0e7404832bbe7a4dbfab82

--- README.md
+++
@@ -1,3 +0,0 @@
-# Welcome to Hypothesis-Bio!
-
-Hypothesis extension for computational biology
--- hypothesis_bio/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""Top-level package for hypothesis-bio."""
-
-__author__ = "Benjamin D. Lee"
-__email__ = "benjamindlee@me.com"
-
-from .__version__ import __version__
-from .hypothesis_bio import *
