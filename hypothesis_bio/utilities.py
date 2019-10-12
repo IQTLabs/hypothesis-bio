9ac61ae374e70e923b4767c373e538152f7c4efc
Benjamin Lee <benjamindlee@me.com>
2019-10-12 19:53:57 -0400

    Add utility for ambiguous bases

---
+++ hypothesis_bio/utilities.py
@@ -0,0 +1,24 @@
+from typing import List
+from itertools import product
+
+ambiguous_bases = {
+    "A": ["A", "W", "M", "R", "D", "H", "V", "N"],
+    "T": ["T", "W", "K", "Y", "B", "D", "H", "N"],
+    "G": ["G", "S", "K", "R", "B", "D", "V", "N"],
+    "C": ["C", "S", "M", "Y", "B", "H", "V", "N"],
+}
+
+
+def ambiguate_seq(seq: str) -> List[str]:
+    return [
+        "".join(prod)
+        for prod in list(product(*[ambiguous_bases[base] for base in seq]))
+    ]
+
+
+ambiguous_start_codons = ambiguate_seq("ATG")
+ambiguous_stop_codons = [
+    ambig_stop
+    for stop_codon in ["TAA", "TAG", "TGA"]
+    for ambig_stop in ambiguate_seq(stop_codon)
+]
