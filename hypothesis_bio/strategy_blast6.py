56ebda0b2babb924cba70aee4d0d77541711a354
Vaastav Anand <vaastav.anand05@gmail.com>
2019-10-19 12:14:32 -0700

    Add initial blast6 strategy file

---
+++ hypothesis_bio/strategy_blast6.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+"""Main module."""
+
+BLAST6_DEFAULT_COL_HEADERS = ["qseqid", "sseqid", "pident", "length", "mismatch", "gapopen", "qstart", "qend", "sstart", "send", "evalue", "bitscore"]
