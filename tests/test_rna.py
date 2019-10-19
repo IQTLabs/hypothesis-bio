8e6d1a49c0e0a09e61c0827450dc40bff17de098
Vaastav Anand <vaastav.anand05@gmail.com>
2019-10-19 02:34:49 -0700

    Add extensive tests for testing various options

--- tests/test_rna.py
+++ tests/test_rna.py
@@ -25,3 +25,39 @@ def test_2_mer():
 @given(rna(max_size=10))
 def test_max_size(seq):
     assert len(seq) <= 10
+
+
+def test_allow_gaps_smallest_example():
+    fn = lambda x: all(c not in ["A", "U", "C", "G"] for c in x)
+    seq = minimal(rna(min_size=1, allow_gaps=True), fn)
+    assert seq == "-"
+
+
+def test_allow_gaps_2_mer():
+    fn = lambda x: all(c not in ["A", "U", "C", "G"] for c in x)
+    seq = minimal(rna(min_size=2, allow_gaps=True), fn)
+    assert seq == "--"
+
+
+def test_allow_lowercase_smallest_example():
+    fn = lambda x: all(c not in ["A", "U", "C", "G"] for c in x)
+    seq = minimal(rna(min_size=1, allow_lowercase=True), fn)
+    assert seq == "a"
+
+
+def test_allow_lowercase_2_mer():
+    fn = lambda x: all(c not in ["A", "U", "C", "G"] for c in x)
+    seq = minimal(rna(min_size=2, allow_lowercase=True), fn)
+    assert seq == "aa"
+
+
+def test_allow_ambiguous_smallest_example():
+    fn = lambda x: all(c not in ["A", "U", "C", "G"] for c in x)
+    seq = minimal(rna(min_size=1, allow_ambiguous=True), fn)
+    assert seq == "B"
+
+
+def test_allow_ambiguous_2_mer():
+    fn = lambda x: all(c not in ["A", "U", "C", "G"] for c in x)
+    seq = minimal(rna(min_size=2, allow_ambiguous=True), fn)
+    assert seq == "BB"
