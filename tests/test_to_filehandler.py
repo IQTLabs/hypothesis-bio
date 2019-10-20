2b82bc0801f35be44838d3a7d18e206be66d933c
Jessica <jessica.h0629@gmail.com>
2019-10-20 11:53:49 -0700

    Update test_to_filehandler.py

--- tests/test_to_filehandler.py
+++ tests/test_to_filehandler.py
@@ -6,17 +6,17 @@ from hypothesis_bio.to_filehandler import test_biopython, test_scikit
 
 @given(fasta())
 def test_scikit_file(seq):
-    assert test_scikit(seq, "fasta") == 1
+    assert test_scikit(seq, "fasta") == True
 
 
 def test_scikit_file_false():
-    assert test_scikit(">\n", "fasta") == 0
+    assert test_scikit(">\n", "fasta") == False
 
 
 @given(fasta())
 def test_biopython_file(seq):
-    assert test_biopython(seq, "fasta") == 1
+    assert test_biopython(seq, "fasta") == True
 
 
 def test_biopython_file_false():
-    assert test_biopython("xsa\nKEOW", "fasta") == 0
+    assert test_biopython("xsa\nKEOW", "fasta") == False
