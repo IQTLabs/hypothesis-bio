ff1410b6f5c8e62920b14de4024b15e917acd100
Reva Shenwai <reva.shenwai96@gmail.com>
2019-10-20 16:37:32 -0400

    Corrected fasta tests #3

--- tests/test_fasta.py
+++ tests/test_fasta.py
@@ -43,7 +43,7 @@ def test_fasta_minimal():
     assert actual == expected
 
 
-@given(fasta(entry_source=fasta_entry(wrap_length=0), min_reads=3, max_reads=3))
+@given(fasta(entry_source=fasta_entry(wrap_length=5), min_reads=3, max_reads=3))
 def test_fasta_min_and_max_reads_the_same_no_wrapping(fasta_file):
     lines = fasta_file.split("\n")
 
